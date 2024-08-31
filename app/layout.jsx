ac43d6aa74cd8d33a1ce6f00504ca04fdcc300e3
Milky-Siraj <milky.siraj@aastustudent.edu.et>
2024-08-31 12:49:51 -0400

    navbar created

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import "@/assets/styles/globals.css";
+import Navbar from "@/components/Navbar";
 export const metadata = {
   title: "PropertyPulse | Find The Perfect Rental",
   desctiption: "Find your dream rental property",
@@ -7,7 +8,8 @@ const MainLayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
-        <div>{children}</div>
+        <Navbar />
+        <main>{children}</main>
       </body>
     </html>
   );
