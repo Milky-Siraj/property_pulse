085ea5296bc955dcf32d2be73ff6356ed5c1e415
Milky-Siraj <milky.siraj@aastustudent.edu.et>
2024-08-31 18:51:43 -0400

    propeties page added

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,7 @@
 import "@/assets/styles/globals.css";
+import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
+
 export const metadata = {
   title: "PropertyPulse | Find The Perfect Rental",
   desctiption: "Find your dream rental property",
@@ -9,7 +11,9 @@ const MainLayout = ({ children }) => {
     <html lang="en">
       <body>
         <Navbar />
+
         <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
