44184d07e4e755a893e6b16457e841d76622765c
Milky-Siraj <milky.siraj@aastustudent.edu.et>
2024-09-05 12:37:59 -0400

    details page done

--- components/PropertyDetails.jsx
+++ components/PropertyDetails.jsx
@@ -86,7 +86,7 @@ const PropertyDetails = ({ property }) => {
         <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 list-none">
           {property.amenities.map((amenity, index) => (
             <li key={index}>
-              <FaCheck className="inline-block text-green-600 mr-2 mt-3" />{" "}
+              <FaCheck className="inline-block text-green-600 mr-2 " />{" "}
               {amenity}
             </li>
           ))}
---
+++ components/Spinner.jsx
@@ -0,0 +1,22 @@
+"use client";
+import React from "react";
+import ClipLoader from "react-spinners/ClipLoader";
+import { useState } from "react";
+const override = {
+  display: "block",
+  margin: "100px auto",
+};
+const Spinner = () => {
+  const [loading, setLoading] = useState(true);
+  return (
+    <ClipLoader
+      color="#3b82f6"
+      loading={loading}
+      cssOverride={override}
+      size={150}
+      aria-label="Loading Spinner"
+    />
+  );
+};
+
+export default Spinner;
