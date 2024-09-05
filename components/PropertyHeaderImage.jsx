e20f98b8d48ef69a013375492c594c49617cb917
Milky-Siraj <milky.siraj@aastustudent.edu.et>
2024-09-05 11:19:33 -0400

    db connection done

--- components/PropertyHeaderImage.jsx
+++ components/PropertyHeaderImage.jsx
@@ -1,7 +1,22 @@
 import React from "react";
-
-const PropertyHeaderImage = () => {
-  return <div>PropertyHeaderImage</div>;
+import Image from "next/image";
+const PropertyHeaderImage = ({ image }) => {
+  return (
+    <section>
+      <div className="container-xl m-auto">
+        <div className="grid grid-cols-1">
+          <Image
+            src={`/images/properties/${image}`}
+            alt=""
+            className="object-cover h-[400px] w-full"
+            width={800}
+            height={600}
+            priority={true}
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default PropertyHeaderImage;
