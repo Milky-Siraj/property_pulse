513a64fa8acff8da69d6fa2190f29450ffc00135
Milky-Siraj <milky.siraj@aastustudent.edu.et>
2024-10-28 10:17:12 -0500

    edit properties done

--- components/PropertyCard.jsx
+++ components/PropertyCard.jsx
@@ -10,7 +10,7 @@ import {
 } from "react-icons/fa";
 
 const PropertyCard = ({ property }) => {
-  const gerateDisplay = () => {
+  const generateDisplay = () => {
     const { rates } = property;
 
     if (rates.monthly) {
@@ -21,6 +21,7 @@ const PropertyCard = ({ property }) => {
       return `${rates.nightly.toLocaleString()}/night`;
     }
   };
+
   return (
     <div className="rounded-xl shadow-md relative">
       <Image
@@ -28,7 +29,6 @@ const PropertyCard = ({ property }) => {
         alt=""
         width={800} // Adjust the width based on your requirement
         height={600}
-        size="100vw"
         className="w-full h-auto rounded-t-xl"
       />
       <div className="p-4">
@@ -37,21 +37,21 @@ const PropertyCard = ({ property }) => {
           <h3 className="text-xl font-bold">{property.name}</h3>
         </div>
         <h3 className="absolute top-[10px] right-[10px] bg-white px-4 py-2 rounded-lg text-blue-500 font-bold text-right md:text-center lg:text-right">
-          $ ${gerateDisplay()}
+          ${generateDisplay()}
         </h3>
 
         <div className="flex justify-center gap-4 text-gray-500 mb-4">
           <p>
             <FaBed className="inline mr-2" />
-            {property.beds} <span className=" lg:inline">Beds</span>
+            {property.beds} <span className="lg:inline">Beds</span>
           </p>
           <p>
             <FaBath className="inline mr-2" /> {property.baths}{" "}
-            <span className=" lg:inline">Baths</span>
+            <span className="lg:inline">Baths</span>
           </p>
           <p>
             <FaRulerCombined className="inline mr-2" />
-            {property.square_feet} <span className=" lg:inline">sqft</span>
+            {property.square_feet} <span className="lg:inline">sqft</span>
           </p>
         </div>
 
@@ -68,7 +68,7 @@ const PropertyCard = ({ property }) => {
           )}
           {property.rates.weekly && (
             <p>
-              <FaMoneyBill className="inline mr-2" /> weekly
+              <FaMoneyBill className="inline mr-2" /> Weekly
             </p>
           )}
         </div>
