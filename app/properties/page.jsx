2524753702da570c3a40785228d6494bd7c643e3
Milky-Siraj <milky.siraj@aastustudent.edu.et>
2024-11-09 09:26:55 -0500

    Properties page refactor

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,17 +1,7 @@
-import PropertyCard from "@/components/PropertyCard";
-import { fetchProperties } from "@/utils/requests";
 import PropertySearchForm from "@/components/PropertySearchForm";
-const PropertiesPage = async () => {
-  let properties = [];
-  try {
-    properties = await fetchProperties();
-    // console.log("Fetched properties:", properties);
-    // properties.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
-  } catch (error) {
-    console.error("Error fetching properties:", error);
-    return <p>Failed to load properties. Please try again later.</p>;
-  }
+import Properties from "@/components/properties";
 
+const PropertiesPage = async () => {
   return (
     <>
       <section className="bg-blue-700 py-4">
@@ -19,23 +9,7 @@ const PropertiesPage = async () => {
           <PropertySearchForm />
         </div>
       </section>
-      {
-        <section className="px-4 py-6">
-          <div className="container-xl lg:container m-auto px-4 py-6">
-            <p>Number of properties fetched: {properties.length}</p>{" "}
-            {/* Temporary check */}
-            {properties.length === 0 ? (
-              <p>No properties found.</p>
-            ) : (
-              <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                {properties.map((property) => (
-                  <PropertyCard key={property._id} property={property} />
-                ))}
-              </div>
-            )}
-          </div>
-        </section>
-      }
+      <Properties />
     </>
   );
 };
